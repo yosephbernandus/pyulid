caeefb4ac1253a210558593ca6347509f27337ac
yosephbernandus <yosephbernandus@gmail.com>
2025-09-16 21:21:01 +0700

    update the ulid structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::{u128, usize};
 
 use pyo3::prelude::*;
+use rand::Rng;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 // Crockford's Base32 alphabet (exclude I, L, O, U)
 const ALPHABET: &[u8; 32] = b"0123456789ABCDEFGHJKMNPQRSTVWXYZ";
@@ -47,17 +49,76 @@ fn decode_base32(encoded: &str) -> PyResult<u128> {
     Ok(result)
 }
 
-// #[pyfunction]
-// fn ulid() -> PyResult<String> {
-//     let mut rng = rand::rng();
-//     let random_number: u64 = rng.random();
-//     Ok(format!("ULID-{:016X}", random_number))
-// }
+#[derive(Debug, Clone, Copy)]
+pub struct Ulid(u128);
+
+impl Ulid {
+    const TIME_BITS: u8 = 48;
+    const RAND_BITS: u8 = 80;
+
+    pub fn from_parts(timestamp_ms: u64, random: u128) -> Self {
+        let time_part = (timestamp_ms as u128) & Self::bitmask(Self::TIME_BITS);
+        let rand_part = random & Self::bitmask(Self::RAND_BITS);
+        Ulid((time_part << Self::RAND_BITS) | rand_part)
+    }
+
+    pub fn timestamp_ms(&self) -> u64 {
+        (self.0 >> Self::RAND_BITS) as u64
+    }
+
+    pub fn random(&self) -> u128 {
+        self.0 & Self::bitmask(Self::RAND_BITS)
+    }
+
+    pub fn to_string(&self) -> String {
+        encode_base32_internal(self.0)
+    }
+
+    const fn bitmask(len: u8) -> u128 {
+        (1 << len) - 1
+    }
+}
+
+fn encode_base32_internal(number: u128) -> String {
+    if number == 0 {
+        return "0".to_string();
+    }
+
+    let mut result = String::new();
+    let mut n = number;
+
+    while n > 0 {
+        let remainder = (n % 32) as usize;
+        result.insert(0, ALPHABET[remainder] as char);
+        n /= 32;
+    }
+
+    while result.len() < 26 {
+        result.insert(0, '0');
+    }
+
+    return result;
+}
+
+#[pyfunction]
+fn ulid() -> PyResult<String> {
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as u64;
+
+    let mut rng = rand::rng();
+    let random: u128 = rng.random::<u128>() & Ulid::bitmask(80);
+
+    let ulid = Ulid::from_parts(timestamp, random);
+    Ok(ulid.to_string())
+}
 //
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pyulid(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(encode_base32, m)?)?;
     m.add_function(wrap_pyfunction!(decode_base32, m)?)?;
+    m.add_function(wrap_pyfunction!(ulid, m)?)?;
     Ok(())
 }
